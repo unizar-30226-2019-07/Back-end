05bea3d45d1558eaa45597e01c5354b1784e9b79
AndrewKM210 <andrewkm210@gmail.com>
2019-03-10 17:02:31 +0100

    Arreglo fallo del identificador de chat

--- src/main/java/chat/Chat.java
+++ src/main/java/chat/Chat.java
@@ -1,12 +1,18 @@
 package main.java.chat;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /*
  * Definicion de un dato de tipo Chat
  */
 @Entity
 public class Chat {
-   
+	
+    @Id // Autogeneracion de IDs
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
 }
 
