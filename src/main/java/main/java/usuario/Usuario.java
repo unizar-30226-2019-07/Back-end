6fc2b83fb7411f0696ec7cdd4c21f99b07a996e4
Rengentable273 <33006404+Regentable239@users.noreply.github.com>
2019-03-21 13:06:38 +0100

    usuarios atributos

--- src/main/java/main/java/usuario/Usuario.java
+++ src/main/java/main/java/usuario/Usuario.java
@@ -9,26 +9,23 @@ import javax.persistence.Id;
 public class Usuario {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
-
-    private String name;
-
+    private Integer idUsuario;
     private String email;
+    private String status;
+    private String first_name;
+    private String last_name;
+    private String gender;
+    private String birth_date;
+    private float location_lat;
+    private float location_lng;
+    private float rating;
 
 	public Integer getId() {
-		return id;
+		return idUsuario;
 	}
 
 	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
+		this.idUsuario = idUsuario;
 	}
 
 	public String getEmail() {
@@ -38,7 +35,71 @@ public class Usuario {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-    
-    
+
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public String getBirth_date() {
+		return birth_date;
+	}
+
+	public void setBirth_date(String birth_date) {
+		this.birth_date = birth_date;
+	}
+	
+	public float getLocation_lat() {
+		return location_lat;
+	}
+
+	public void setLocation_lat(float location_lat) {
+		this.location_lat = location_lat;
+	}
+	
+	public float getLocation_lng() {
+		return location_lng;
+	}
+
+	public void setLocation_lng(float location_lng) {
+		this.location_lng = location_lng;
+	}
+	
+	public float getRating() {
+		return location_lat;
+	}
+
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+	
 }
 
