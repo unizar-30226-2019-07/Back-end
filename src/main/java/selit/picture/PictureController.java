fd99b4974f0984230007c46780945264ca26b692
victorpowah <g3victorrodriguez@gmail.com>
2019-04-15 01:32:54 +0200

    mime dinamico

--- src/main/java/selit/picture/PictureController.java
+++ src/main/java/selit/picture/PictureController.java
@@ -10,16 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
  	
@@ -36,12 +31,8 @@ public class PictureController {
 		if(p.isPresent()) {
 			Picture pic = p.get();
 			InputStream is = new ByteArrayInputStream(pic.getBase64());
-			if(pic.getMime().equals("image/jpeg")) {
-				response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-			}
-			else {
-				response.setContentType(MediaType.IMAGE_PNG_VALUE);
-			}
+			response.setContentType(pic.getMime());
+
 		    IOUtils.copy(is, response.getOutputStream());		
 		}
 
