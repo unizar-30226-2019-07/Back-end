f16bd165511a095835836921ed3fab40a435968b
AndrewKM210 <andrewkm210@gmail.com>
2019-04-30 22:59:47 +0200

    Optimizado imports

--- src/main/java/selit/auctions/SubastaRepository.java
+++ src/main/java/selit/auctions/SubastaRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import selit.producto.Anuncio;
 
 public interface SubastaRepository extends JpaRepository<Subasta, Long>{
 	
